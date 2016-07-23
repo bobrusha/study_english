465c8156bc1af1d7425033c1b9b79880341a9d2b
Aleksandra Bobrova <bobrova.aleksand@gmail.com>
2016-07-23 14:38:50 +0300

    Finished toolbar.

--- app/src/main/java/com/bobrusha/android/yandex/english/ui/activity/MainActivity.java
+++ app/src/main/java/com/bobrusha/android/yandex/english/ui/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.bobrusha.android.yandex.english.ui.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bobrusha.android.yandex.english.R;
 import com.bobrusha.android.yandex.english.ui.fragment.MainFragment;
@@ -16,14 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.fragment_content, new MainFragment())
                     .commit();
+            ((TextView)findViewById(R.id.toolbar_title)).setText(R.string.main_title);
+            ((ImageView)findViewById(R.id.settings)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.v("qq", "qq");
+                }
+            });
         }
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        final ActionBar ab = getSupportActionBar();
     }
 }
